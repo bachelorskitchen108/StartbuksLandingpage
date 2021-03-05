ad9c9d7033131041c295ee0993974c8140131470
bachelore Kitchen <77671705+bachelorskitchen108@users.noreply.github.com>
2021-03-05 19:29:05 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+function imgSlider(e) {
+     document.querySelector(".sb").src = e;
+}
+function changeCircleColor(color) {
+     const circleColor = document.querySelector(".circle");
+     circleColor.style.background = color;
+}
